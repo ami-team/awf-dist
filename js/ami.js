4a75c00c9b519d85d7ad718be86d5b7a89eb59d6
Jérôme ODIER <ami@lpsc.in2p3.fr>
2023-10-06 15:26:48 +0000

    update

--- js/ami.js
+++ js/ami.js
@@ -35848,7 +35848,8 @@ class AMIWebApp {
     return null;
   }
   start(options) {
-    amiWebApp.themeSet(localStorage.getItem('theme') || 'light');
+    const theme = localStorage.getItem('theme') || 'light';
+    amiWebApp.themeSet(theme);
     this.#globalDeferred.done(() => {
       const [logoURL, backgroundLightURL, backgroundDarkURL, signInImageURL, homeURL, contactEmail, aboutURL, defaultThemeURL, dashboardThemeURL, lockerURL, endpointURL, signInText, ssoAutoAuthentication, ssoAuthenticationAllowed, passwordAuthenticationAllowed, certificateAuthenticationAllowed, signOutAllowed, createAccountAllowed, changeInfoAllowed, changePasswordAllowed, changeCertificateAllowed, captchaAllowed, dayNightAllowed, bookmarksAllowed, dashboardsAllowed] = setup(['logo_url', 'background_light_url', 'background_dark_url', 'sign_in_image_url', 'home_url', 'contact_email', 'about_url', 'default_theme_url', 'dashboard_theme_url', 'locker_url', 'endpoint_url', 'sign_in_text', 'sso_auto_authentication', 'sso_authentication_allowed', 'password_authentication_allowed', 'certificate_authentication_allowed', 'sign_out_allowed', 'create_account_allowed', 'change_info_allowed', 'change_password_allowed', 'change_certificate_allowed', 'captcha_allowed', 'day_night_allowed', 'bookmarks_allowed', 'dashboards_allowed'], [logo_namespaceObject, background_light_namespaceObject, background_dark_namespaceObject, sign_in_image_namespaceObject, this.webAppURL, 'ami@lpsc.in2p3.fr', 'https://cern.ch/ami/', `${this.originURL}/twig/v${this.bootstrapVersion}/Themes/blue.twig`, `${this.originURL}/twig/v${this.bootstrapVersion}/Themes/cloud.twig`, `${this.originURL}/twig/v${this.bootstrapVersion}/Lockers/default.twig`, `${this.originURL}/AMI/FrontEnd`, 'Welcome in AMI!', false, false, true, true, true, true, true, true, true, true, true, true, true], options);
       js_AMICommand.initHttpClient(endpointURL);
@@ -35956,8 +35957,10 @@ class AMIWebApp {
     document.documentElement.setAttribute('data-bs-theme', theme);
     const el = $('label[for="B3094B72_95CB_0F9F_0364_0ED6B6DCD6EF"] i');
     if (theme === 'dark') {
+      $('#B3094B72_95CB_0F9F_0364_0ED6B6DCD6EF').prop('select', true);
       el.addClass('bi-moon-stars-fill').removeClass('bi-sun-fill');
     } else {
+      $('#B3094B72_95CB_0F9F_0364_0ED6B6DCD6EF').prop('select', false);
       el.removeClass('bi-moon-stars-fill').addClass('bi-sun-fill');
     }
     localStorage.setItem('theme', theme);
