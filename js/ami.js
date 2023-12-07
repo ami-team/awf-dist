01419a90f566ddcac21869f953bb7c9c4ad3538d
Jérôme ODIER <ami@lpsc.in2p3.fr>
2023-12-07 09:20:05 +0000

    update

--- js/ami.js
+++ js/ami.js
@@ -11789,6 +11789,7 @@ class Text {
     return this.lineInner(n, true, 1, 0);
   }
   replace(from, to, text) {
+    [from, to] = clip(this, from, to);
     let parts = [];
     this.decompose(0, from, parts, 2);
     if (text.length) text.decompose(0, text.length, parts, 1 | 2);
@@ -11802,6 +11803,7 @@ class Text {
     if (to === void 0) {
       to = this.length;
     }
+    [from, to] = clip(this, from, to);
     let parts = [];
     this.decompose(from, to, parts, 0);
     return TextNode.from(parts, to - from);
@@ -11901,6 +11903,7 @@ class TextLeaf extends Text {
   }
   replace(from, to, text) {
     if (!(text instanceof TextLeaf)) return super.replace(from, to, text);
+    [from, to] = clip(this, from, to);
     let lines = appendText(this.text, appendText(text.text, sliceText(this.text, 0, from)), to);
     let newLen = this.length + text.length - (to - from);
     if (lines.length <= 32) return new TextLeaf(lines, newLen);
@@ -11913,6 +11916,7 @@ class TextLeaf extends Text {
     if (lineSep === void 0) {
       lineSep = "\n";
     }
+    [from, to] = clip(this, from, to);
     let result = "";
     for (let pos = 0, i = 0; pos <= to && i < this.text.length; i++) {
       let line = this.text[i],
@@ -11982,6 +11986,7 @@ class TextNode extends Text {
     }
   }
   replace(from, to, text) {
+    [from, to] = clip(this, from, to);
     if (text.lines < this.lines) for (let i = 0, pos = 0; i < this.children.length; i++) {
       let child = this.children[i],
         end = pos + child.length;
@@ -12006,6 +12011,7 @@ class TextNode extends Text {
     if (lineSep === void 0) {
       lineSep = "\n";
     }
+    [from, to] = clip(this, from, to);
     let result = "";
     for (let i = 0, pos = 0; i < this.children.length && pos <= to; i++) {
       let child = this.children[i],
@@ -12291,6 +12297,10 @@ class Line {
   get length() {
     return this.to - this.from;
   }
+}
+function clip(text, from, to) {
+  from = Math.max(0, Math.min(text.length, from));
+  return [from, Math.max(from, Math.min(text.length, to))];
 }
 let extend = "lc,34,7n,7,7b,19,,,,2,,2,,,20,b,1c,l,g,,2t,7,2,6,2,2,,4,z,,u,r,2j,b,1m,9,9,,o,4,,9,,3,,5,17,3,3b,f,,w,1j,,,,4,8,4,,3,7,a,2,t,,1m,,,,2,4,8,,9,,a,2,q,,2,2,1l,,4,2,4,2,2,3,3,,u,2,3,,b,2,1l,,4,5,,2,4,,k,2,m,6,,,1m,,,2,,4,8,,7,3,a,2,u,,1n,,,,c,,9,,14,,3,,1l,3,5,3,,4,7,2,b,2,t,,1m,,2,,2,,3,,5,2,7,2,b,2,s,2,1l,2,,,2,4,8,,9,,a,2,t,,20,,4,,2,3,,,8,,29,,2,7,c,8,2q,,2,9,b,6,22,2,r,,,,,,1j,e,,5,,2,5,b,,10,9,,2u,4,,6,,2,2,2,p,2,4,3,g,4,d,,2,2,6,,f,,jj,3,qa,3,t,3,t,2,u,2,1s,2,,7,8,,2,b,9,,19,3,3b,2,y,,3a,3,4,2,9,,6,3,63,2,2,,1m,,,7,,,,,2,8,6,a,2,,1c,h,1r,4,1c,7,,,5,,14,9,c,2,w,4,2,2,,3,1k,,,2,3,,,3,1m,8,2,2,48,3,,d,,7,4,,6,,3,2,5i,1m,,5,ek,,5f,x,2da,3,3x,,2o,w,fe,6,2x,2,n9w,4,,a,w,2,28,2,7k,,3,,4,,p,2,5,,47,2,q,i,d,,12,8,p,b,1a,3,1c,,2,4,2,2,13,,1v,6,2,2,2,2,c,,8,,1b,,1f,,,3,2,2,5,2,,,16,2,8,,6m,,2,,4,,fn4,,kh,g,g,g,a6,2,gt,,6a,,45,5,1ae,3,,2,5,4,14,3,4,,4l,2,fx,4,ar,2,49,b,4w,,1i,f,1k,3,1d,4,2,2,1x,3,10,5,,8,1q,,c,2,1g,9,a,4,2,,2n,3,2,,,2,6,,4g,,3,8,l,2,1l,2,,,,,m,,e,7,3,5,5f,8,2,3,,,n,,29,,2,6,,,2,,,2,,2,6j,,2,4,6,2,,2,r,2,2d,8,2,,,2,2y,,,,2,6,,,2t,3,2,4,,5,77,9,,2,6t,,a,2,,,4,,40,4,2,2,4,,w,a,14,6,2,4,8,,9,6,2,3,1a,d,,2,ba,7,,6,,,2a,m,2,7,,2,,2,3e,6,3,,,2,,7,,,20,2,3,,,,9n,2,f0b,5,1n,7,t4,,1r,4,29,,f5k,2,43q,,,3,4,5,8,8,2,7,u,4,44,3,1iz,1j,4,1e,8,,e,,m,5,,f,11s,7,,h,2,7,,2,,5,79,7,c5,4,15s,7,31,7,240,5,gx7k,2o,3k,6o".split(",").map(s => s ? parseInt(s, 36) : 1);
 for (let i = 1; i < extend.length; i++) extend[i] += extend[i - 1];
@@ -35836,7 +35846,6 @@ function _setupCtx(ctxImmutables, ctxDefaults, ctxOptions, ctx, immutables, defa
         options['dict'] = {};
       }
       options.dict.ctx = this.ctx;
-      options.dict.instance = this;
       options.scope = this._instanceScope;
       return prependHTML(selector, twig, options);
     },
@@ -35848,7 +35857,6 @@ function _setupCtx(ctxImmutables, ctxDefaults, ctxOptions, ctx, immutables, defa
         options['dict'] = {};
       }
       options.dict.ctx = this.ctx;
-      options.dict.instance = this;
       options.scope = this._instanceScope;
       return appendHTML(selector, twig, options);
     },
